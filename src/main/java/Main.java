206aca156a25abf1d5ebfe6d1ff199f965296a38
Alex Russell <128105099+AlexVRussell@users.noreply.github.com>
2026-08-16 20:11:21 -0300

    add updating logic for filling match weeks in spread sheet

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,63 @@
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.List;
-import io.github.cdimascio.dotenv.Dotenv;
 
 public class Main {
     public static void main(String[] args) {
+
         Config config = new Config();
         FootballApi footballApi = new FootballApi(config);
         GoogleSheets googleSheets = new GoogleSheets(config);
-        int row = 2;
 
-        List<Match> matches = footballApi.getFixtures(LocalDate.of(2026, 8, 21), LocalDate.of(2026, 8, 24));
+        LocalDate today = LocalDate.of(2026, 8, 23);
+
+        LocalDate from;
+        LocalDate to;
+
+        if (today.getDayOfWeek() == DayOfWeek.MONDAY) {
+            from = today.minusDays(3);
+            to = today;
+        } else {
+            from = today.minusDays(
+                    today.getDayOfWeek().getValue() - DayOfWeek.FRIDAY.getValue()
+            );
+            to = from.plusDays(3);
+        }
+
+        System.out.println("Today: " + today);
+        System.out.println("From: " + from);
+        System.out.println("To: " + to);
+
+        List<Match> matches = footballApi.getFixtures(from, to);
+
+        int row = 2;
 
         for (Match match : matches) {
             if (match.getLeague().getLeagueId() != 15) {
                 continue;
             }
 
-            System.out.println("Match: " + match.getHomeTeam().getTeamName() + " vs " + match.getAwayTeam().getTeamName() + " - Score: " +  match.getScore() + " - Date: " + match.getMatchDate());
-            googleSheets.updateRow(row, match.getHomeTeam().getTeamName() + " vs " + match.getAwayTeam().getTeamName(), match.getStatus(), match.getMatchDate(), match.getScore());
+            System.out.println(
+                    "Match: " +
+                            match.getHomeTeam().getTeamName() +
+                            " vs " +
+                            match.getAwayTeam().getTeamName() +
+                            " - Score: " +
+                            match.getScore() +
+                            " - Date: " +
+                            match.getMatchDate()
+            );
+
+            googleSheets.updateRow(
+                    row,
+                    match.getHomeTeam().getTeamName() +
+                            " vs " +
+                            match.getAwayTeam().getTeamName(),
+                    match.getStatus(),
+                    match.getMatchDate(),
+                    match.getScore()
+            );
+
             row++;
         }
     }
